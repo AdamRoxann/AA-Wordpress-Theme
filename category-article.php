9252c5cb816e319a3fe728a32f81489515ab9ba1
AdamRoxann <adamtraceurs@gmail.com>
2024-10-03 17:21:35 +0700

    feat: :art: Update color

--- category-article.php
+++ category-article.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="container">
-    <h3 class="my-3 font-bold"><?php echo single_cat_title(); ?></h3>
+    <h2 class="my-3 font-bold primary-color"><?php echo single_cat_title(); ?></h2>
     <?php get_template_part('includes/section', 'archive'); ?>
     <p class="float-end nav-link text-white text-decoration-none"><?php the_posts_pagination(); ?></p>
 </div>
