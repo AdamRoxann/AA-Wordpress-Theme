bdeb859609755be9dfc078e5973a698ef9699b51
AdamRoxann <adamtraceurs@gmail.com>
2024-10-29 11:25:00 +0700

    feat: Update keyword

--- front-page.php
+++ front-page.php
@@ -2,6 +2,15 @@
 
 <div class="">
     <?php get_template_part('includes/section', 'content'); ?>
+    <div style="display: flex; justify-content: center; align-items: center;">
+        <h2 style="color: #fff; font-size: 0.2px">Alexandra Askandar</h2>
+        <h2 style="color: #fff; font-size: 0.2px">⁠⁠Alexandra Askandar Bank Mandiri</h2>
+        <h2 style="color: #fff; font-size: 0.2px">Alexandra Askandar Wadirut</h2>
+        <h2 style="color: #fff; font-size: 0.2px">Alexandra Askandar Perempuan Inspiratif</h2>
+        <h2 style="color: #fff; font-size: 0.2px">Profil Alexandra Askandar</h2>
+        <h2 style="color: #fff; font-size: 0.2px">⁠Biodata Alexandra Askandar</h2>
+        <h2 style="color: #fff; font-size: 0.2px">Prestasi Alexandra Askandar</h2>
+    </div>
 </div>
 
 <?php get_footer(); ?>
